55e0e5447966be8f2c98e773e2d47b2c9c8c0bed
Mario Kovac <mkovac25122512@gmail.com>
2023-04-15 23:07:25 +0200

    Add animations to tech stack & hero sections

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,34 @@
 import "./Hero.scss";
 
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const variants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 },
+};
+
 const Hero = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
   return (
-    <section className="hero-section">
+    <motion.section
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={variants}
+      className="hero-section"
+    >
       <div className="hero-section__text-wrapper">
         <h3 className="hero-section__h3">it's a me,</h3>
         <h1 className="hero-section__h1">MARIO KOVAČ</h1>
@@ -10,7 +36,7 @@ const Hero = () => {
           design enthusiast, gamer, bookworm, film geek
         </h4>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- src/components/tech-stack/TechStack.tsx
+++ src/components/tech-stack/TechStack.tsx
@@ -1,13 +1,39 @@
 import "./TechStack.scss";
 
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import { AiFillHtml5, AiFillGithub } from "react-icons/ai";
 import { DiCss3, DiReact, DiSass, DiNodejsSmall } from "react-icons/di";
 import { IoLogoJavascript } from "react-icons/io";
 import { SiTypescript, SiDocker, SiKubernetes, SiFigma } from "react-icons/si";
 
+const variants = {
+  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  hidden: { opacity: 0, x: -200 },
+};
+
 const TechStack = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
   return (
-    <section className="tech-stack-section">
+    <motion.section
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={variants}
+      className="tech-stack-section"
+    >
       <div className="tech-stack-section__content">
         <AiFillHtml5 size={60} className="tech-stack__item" />
         <AiFillGithub size={60} className="tech-stack__item" />
@@ -21,7 +47,7 @@ const TechStack = () => {
         <SiKubernetes size={50} className="tech-stack__item" />
         <SiFigma size={50} className="tech-stack__item" />
       </div>
-    </section>
+    </motion.section>
   );
 };
 
