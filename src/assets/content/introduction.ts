80fb0b5fc88292140dd1db4e1163d5c07b3ed511
Mario Kovac <mkovac25122512@gmail.com>
2023-04-21 20:39:05 +0200

    Introduction typo fix

--- src/assets/content/introduction.ts
+++ src/assets/content/introduction.ts
@@ -1,7 +1,7 @@
 export const introduction = {
   h1: "I AM A JUNIOR FRONTEND DEVELOPER",
   p: "Hi, I am an aspiring junior Frontend developer who currently works in Split, \
-  Croatia as a Software Engineer. I am really passionate about everything web related, \
+  Croatia as a Software developer. I am really passionate about everything web related, \
   from designing even the smallest of buttons and making the simplest of animations \
   all the way to creating a little bit more complex websites and applications.",
 };
